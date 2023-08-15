6a562fb1357df843b7e03adf5c1332d3b8fed32c
Bob Belderbos <bobbelderbos@gmail.com>
2023-08-15 18:46:07 +0200

    simple user interface

--- script.py
+++ script.py
@@ -1,16 +1,30 @@
-from googletrans import Translator
+from googletrans import Translator, LANGUAGES
 
 
 def main():
-    print(translate_text("hello world", target="fr"))
+    while True:
+        target = input("Choose a language to translate to (type 'q' to exit): ")
+        if target == "q":
+            break
+        if target not in LANGUAGES:
+            print(f'Invalid target language, valid are: {", ".join(LANGUAGES)}')
+            continue
+
+        while True:
+            text = input(
+                f"Enter text to translate to {LANGUAGES[target]} (type 'q' to change language): "
+            )
+            if text == "q":
+                break
+            translated = translate_text(text, target=target)
+            print(translated)
 
 
-def translate_text(text, target='en'):
+def translate_text(text, target="en"):
     translator = Translator()
     translation = translator.translate(text, dest=target)
     return translation.text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
